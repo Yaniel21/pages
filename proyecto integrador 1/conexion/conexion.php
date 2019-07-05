cad552b7d72dfa32268b78e1aa0dd0d0bd03cda9
Yaniel21 <profeyani21@gmail.com>
2019-07-05 10:40:56 -0300

    primer commit version00

---
+++ proyecto integrador 1/conexion/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$enlace = mysqli_connect('localhost', 'root', '', 'juegos_programacion');
+
+if (!$enlace){
+ exit("Error de Conexión ". mysqli_connect_error());
+ "<script> alert('No se pudo conectar') </script>"
+ }
+?>
